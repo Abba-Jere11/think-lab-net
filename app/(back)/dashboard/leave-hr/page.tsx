aa7c5a4e5ee200a6fe26e3fae6779987ac1fce70
Abba-jere11 <itz0jere@gmail.com>
2025-07-19 20:26:41 +0100

    fixed

--- app/(back)/dashboard/leave-hr/page.tsx
+++
@@ -1,5 +0,0 @@
-import LeaveManagementPage from "@/leave-management-page"
-
-export default function Page() {
-  return <LeaveManagementPage />
-}
--- app/(back)/dashboard/leave/page.tsx
+++
@@ -1,6 +0,0 @@
-import { StaffLeavePortal } from "@/components/staff-leave-portal";
-
-
-export default function Page() {
-  return <StaffLeavePortal />
-}
